2638c2ee75aef4ca9712f424a9e9a131c9d5ad31
Koidnulu <sunlanxi2008@gmail.com>
2026-09-12 22:54:19 -0700

    fixed coin issue

--- jump-start/coin.gd
+++ jump-start/coin.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+func _physics_process(delta: float) -> void:
+	sprite.play("default")
 
-func _on_body_entered(body: CharacterBody2D) -> void:
-	pass
+func _on_body_entered(body: Node2D) -> void:
+	animation_player.play("pickup")
